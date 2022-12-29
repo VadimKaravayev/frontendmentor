faf23227be7681527b47829755878397523bd4c4
vadymkaravayev <vadym_karavayev_tmg@whirlpool.com>
2022-12-29 18:31:10 +0200

    rating style

---
+++ interactive-rating-component-main/scripts.js
@@ -0,0 +1,15 @@
+{
+  /* <script>
+      const btn = document.querySelector("#form-id");
+      btn.addEventListener("submit", (e) => {
+        e.preventDefault();
+        // const form = e.target;
+        // const val = form.elements["foo"].value;
+        const form = new FormData(e.target);
+        const val = form.get("foo");
+        console.log("You chose ", val);
+      });
+    </script> */
+}
+
+console.log("hello world");
