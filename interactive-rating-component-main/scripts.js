3283d7a6da316f8faaa0e930ea1e04c43a5a889a
vadymkaravayev <vadym_karavayev_tmg@whirlpool.com>
2022-12-31 21:52:16 +0200

    finished rating widget

--- interactive-rating-component-main/scripts.js
+++ interactive-rating-component-main/scripts.js
@@ -1,15 +1,62 @@
-{
-  /* <script>
-      const btn = document.querySelector("#form-id");
-      btn.addEventListener("submit", (e) => {
-        e.preventDefault();
-        // const form = e.target;
-        // const val = form.elements["foo"].value;
-        const form = new FormData(e.target);
-        const val = form.get("foo");
-        console.log("You chose ", val);
-      });
-    </script> */
-}
+(function (doc) {
+  function init() {
+    doc.addEventListener("click", restartRating);
 
-console.log("hello world");
+    const form = get("#rating-form");
+    form.addEventListener("submit", handleSubmit);
+  }
+
+  function restartRating(event) {
+    const thankyouWindow = get(".thankyou-window");
+    const ratingWindow = get(".rating-window");
+
+    if (
+      !thankyouWindow.contains(event.target) &&
+      ratingWindow.classList.contains("hide")
+    ) {
+      const checkedRadio = get("input[type=radio]:checked");
+      checkedRadio.checked = false;
+      removeErrorMsg(".error-msg");
+      thankyouWindow.classList.add("hide");
+      ratingWindow.classList.remove("hide");
+    }
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    removeErrorMsg(".error-msg");
+    const data = new FormData(event.target);
+    const value = data.get("rating");
+    if (!value) {
+      event.target.append(createErrorMsg("You must rate before submit"));
+      return;
+    }
+    const ratingWindow = get(".rating-window");
+    ratingWindow.classList.add("hide");
+
+    console.log("You chose ", value);
+    const result = get(".rate-value");
+    result.innerText = value;
+
+    const thankyouWindow = get(".thankyou-window");
+    thankyouWindow.classList.remove("hide");
+  }
+
+  function removeErrorMsg(selector) {
+    const errorMgs = get(selector);
+    errorMgs && errorMgs.remove();
+  }
+
+  function createErrorMsg(text) {
+    const msg = doc.createElement("span");
+    msg.classList.add("error-msg");
+    msg.innerText = text;
+    return msg;
+  }
+
+  function get(selector) {
+    return document.querySelector(selector);
+  }
+
+  init();
+})(document);
