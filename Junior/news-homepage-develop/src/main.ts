b5a856bdb45d65be1104c7b73de01319b70a0fe5
vadymkaravayev <vadym_karavayev_tmg@whirlpool.com>
2023-11-26 19:50:10 +0200

    finished news-home-page

--- Junior/news-homepage-develop/src/main.ts
+++ Junior/news-homepage-develop/src/main.ts
@@ -1,3 +1,12 @@
-import "./less/main.less";
+import './less/main.less';
 
-console.log('main.ts is working');
+const burger = document.querySelector('.mobile-nav-toggle');
+const primaryNavigation = document.querySelector(
+  '.primary-navigation'
+) as HTMLElement;
+
+burger?.addEventListener('click', () => {
+  const isVisibile = String(!(primaryNavigation.dataset.visible === 'true'));
+  primaryNavigation.dataset.visible = isVisibile;
+  burger.setAttribute('aria-expanded', isVisibile);
+});
--- Junior/news-homepage-develop/webpack.config.js
+++ Junior/news-homepage-develop/webpack.config.js
@@ -1,13 +1,13 @@
-const path = require("path");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  mode: "development",
-  entry: "./src/main.ts",
+  mode: 'development',
+  entry: './src/main.ts',
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-    publicPath: "/dist/",
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/dist/',
   },
   devServer: {
     static: [{ directory: path.join(__dirname) }],
@@ -16,18 +16,18 @@ module.exports = {
     rules: [
       {
         test: /\.ts$/,
-        use: "ts-loader",
+        use: 'ts-loader',
         exclude: /node_modules/,
       },
       {
         test: /\.less$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "less-loader"],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'less-loader'],
       },
     ],
   },
   resolve: {
-    extensions: [".ts", ".js"],
+    extensions: ['.ts', '.js'],
   },
-  devtool: "inline-source-map",
-  plugins: [new MiniCssExtractPlugin({ filename: "styles.css" })],
+  devtool: 'inline-source-map',
+  plugins: [new MiniCssExtractPlugin({ filename: 'styles.css' })],
 };
